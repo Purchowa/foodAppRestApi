6f925310d53f0b180794819c5df372584eb42424
Dawid_W <purchowa@gmail.com>
2023-03-19 14:28:27 +0100

    Testing - adding files jdk 17, spring web service

---
+++ src/main/java/com/example/foodAppRS/FoodAppRsApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodAppRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodAppRS/FoodAppRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodAppRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
