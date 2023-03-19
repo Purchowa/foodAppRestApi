ebfab603c39b5ce901b3965b449d7bdcc731bb38
Dawid_W <purchowa@gmail.com>
2023-03-19 14:49:47 +0100

    Testing - rest controller

--- src/main/java/com/example/foodAppRS/FoodAppRsApplication.java
+++ src/main/java/com/example/foodAppRS/FoodAppRsApplication.java
@@ -2,12 +2,22 @@ package com.example.foodAppRS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FoodAppRsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodAppRsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld(){
+		return "Hello here foodRestApi - jdk17";
 	}
 
 }
